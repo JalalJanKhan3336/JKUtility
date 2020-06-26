962ed94cdecbe35493e5b51a4839601ae0d9a490
JalalJanKhan <jalalkhan1193@gmail.com>
2020-06-27 01:19:52 +0500

    Circular Image feature added.

---
+++ jkutils/src/main/java/com/thesoftparrot/jkutils/utils/GlideGIFUtils.java
@@ -0,0 +1,249 @@
+package com.thesoftparrot.jkutils.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.bumptech.glide.Glide;
+
+public class GlideGIFUtils {
+
+    private GlideGIFUtils(){}
+
+    public static void loadImage(Context context, String image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Activity context, String image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(FragmentActivity context, String image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Fragment context, String image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(View context, String image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    // Load image via Uri
+    public static void loadImage(Context context, Uri image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Activity context, Uri image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(FragmentActivity context, Uri image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Fragment context, Uri image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(View context, Uri image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    // Load image via Drawable
+    public static void loadImage(Context context, Drawable image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Activity context, Drawable image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(FragmentActivity context, Drawable image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Fragment context, Drawable image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(View context, Drawable image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    // Load image via resId
+    public static void loadImage(Context context, int image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Activity context, int image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(FragmentActivity context, int image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Fragment context, int image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(View context, int image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    // Load image via Bitmap
+    public static void loadImage(Context context, Bitmap image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Activity context, Bitmap image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(FragmentActivity context, Bitmap image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(Fragment context, Bitmap image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+    public static void loadImage(View context, Bitmap image, int placeholder, ImageView holder){
+        Glide
+                .with(context)
+                .asGif()
+                .load(image)
+                .placeholder(placeholder)
+                .into(holder);
+    }
+
+}
